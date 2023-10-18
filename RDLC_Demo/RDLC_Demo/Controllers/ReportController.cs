dc2ac9aa220689d310f6ac851fe96d4d34564100
dyells07 <bhimkhanal999@gmail.com>
2023-10-18 08:56:33 +0545

    Commit

--- RDLC_Demo/RDLC_Demo/Controllers/ReportController.cs
+++ RDLC_Demo/RDLC_Demo/Controllers/ReportController.cs
@@ -1,6 +1,4 @@
-﻿using AspNetCore.Reporting;
-using Bytescout.BarCode;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using RDLC_Demo.Repositories;
 using System;
@@ -9,6 +7,8 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Reporting.WebForms;
+using Bytescout.BarCode;
 
 namespace RDLC_Demo.Controllers
 {
@@ -21,48 +21,77 @@ namespace RDLC_Demo.Controllers
         {
             _webHostEnvironment = webHostEnvironment ?? throw new ArgumentNullException(nameof(webHostEnvironment));
             _voucherRepository = voucherRepository ?? throw new ArgumentNullException(nameof(voucherRepository));
-            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
         }
 
         public async Task<IActionResult> IndexAsync()
         {
             string mimeType = "application/pdf";
             int extension = 1;
-            var path = Path.Combine(_webHostEnvironment.WebRootPath, "Reports", "Report1.rdlc");
+            var reportPath = Path.Combine(_webHostEnvironment.WebRootPath, "Reports", "Report1.rdlc");
 
             Dictionary<string, string> parameters = new Dictionary<string, string>
             {
-                 {"CName", "Example Company"},
-  {"CAddress", "RatnaChowk, Pokhara"},
-  {"VType", "JOURNAL VOUCHER"},
-  {"CostCenter", "Main"},
-  {"Date", "2080-8-23"},
-  {"Vcode", "234"},
-  {"PName", "Being Paid Amount to Mr. Anunnya Baral from Cheque No:39667472"},
-  {"PBBy", "Kumar Gurung"}
+                {"CName", "Example Company"},
+                {"CAddress", "RatnaChowk, Pokhara"},
+                {"VType", "JOURNAL VOUCHER"},
+                {"CostCenter", "Main"},
+                {"Date", "2080-8-23"},
+                {"Vcode", "234"},
+                {"PName", "Being Paid Amount to Mr. Anunnya Baral from Cheque No:39667472"},
+                {"PBBy", "Kumar Gurung"}
             };
 
             var barcodeValue = "2345";
-            Barcode barcode = new Barcode();
-            barcode.Symbology = SymbologyType.Code128;
-            barcode.Value = barcodeValue;
-            barcode.Margins = new Margins(50, 50, 0, 0);
-            Image barcodeImage = barcode.GetImage();
+            var barcodeImage = GenerateBarcodeImage(barcodeValue);
 
-            using (MemoryStream stream = new MemoryStream())
+            // Convert the barcode image to Base64 string
+            string barcodeBase64;
+            using (var stream = new MemoryStream())
             {
                 barcodeImage.Save(stream, ImageFormat.Png);
-                var barcodeBase64 = Convert.ToBase64String(stream.ToArray());
-                parameters.Add("VBarcode", barcodeBase64);
+                barcodeBase64 = Convert.ToBase64String(stream.ToArray());
             }
 
-            var Voucher = await _voucherRepository.GetVoucherDetail();
+            parameters.Add("VBarcode", barcodeBase64);
 
-            LocalReport localReport = new LocalReport(path);
-            localReport.AddDataSource("Report", Voucher);
-            var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimeType);
+            var voucherData = await _voucherRepository.GetVoucherDetail();
 
-            return File(result.MainStream, mimeType);
+            var reportResult = GenerateReport(reportPath, voucherData, parameters, extension, mimeType);
+
+            return new FileContentResult(reportResult, mimeType)
+            {
+                FileDownloadName = "Report.pdf"
+            };
+        }
+
+        private Image GenerateBarcodeImage(string value)
+        {
+            var barcode = new Barcode
+            {
+                Symbology = BarcodeSymbology.Code128,
+                Value = value,
+                Margins = new Margins(50, 50, 0, 0)
+            };
+
+            return barcode.GetImage();
+        }
+
+        private byte[] GenerateReport(string reportPath, object dataSource, Dictionary<string, string> parameters, int extension, string mimeType)
+        {
+            var reportViewer = new ReportViewer { ProcessingMode = ProcessingMode.Local, LocalReportPath = reportPath };
+
+            // Set the report data source
+            reportViewer.LocalReport.DataSources.Add(new ReportDataSource("Report", dataSource));
+
+            // Set the report parameters
+            foreach (var parameter in parameters)
+            {
+                reportViewer.LocalReport.SetParameters(new ReportParameter(parameter.Key, parameter.Value));
+            }
+
+            var reportBytes = reportViewer.LocalReport.Render(extension.ToString(), null, out var fileExtension, out var encoding, out var mimeType, out var warnings);
+
+            return reportBytes;
         }
     }
 }
